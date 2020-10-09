31bca9ae68cbb6bf5df52656545072c92ebef6b5
Ulrik Hindø <ulhin@tdc.dk>
2020-10-09 09:36:46 +0200

    First commit

---
+++ ulriks-dotnet-core-hello-world-app/Controllers/UlriksHelloAppController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ulriks_dotnet_core_hello_world_app.Controllers
+{
+    // https://localhost:5001/UlriksHelloApp
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UlriksHelloAppController : ControllerBase
+    {
+        private readonly ILogger<UlriksHelloAppController> _logger;
+
+        public UlriksHelloAppController(ILogger<UlriksHelloAppController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var rng = new Random();
+            return new List<string> { "Ulrik", "says", "hello" };
+        }
+    }
+}
